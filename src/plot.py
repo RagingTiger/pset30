93d0fa6e45fb3002fa7b075ebce8ee10ff180b42
RagingTiger <RagingTiger@yourmom.com>
2020-04-09 18:29:27 -0400

    Flash stash

--- src/plot.py
+++ src/plot.py
@@ -1,8 +1,9 @@
 # Import our modules that we are using
+import math
 import matplotlib.pyplot as plt
 import numpy as np
 
-n = 2
+n = 1
 m = 1
 x = np.array(range(-20, 20))
 
@@ -20,6 +21,10 @@ y3 = x**2 - 2 + x
 y4 = x**2 - 2 - x
 y5 = x**2 + 2*x*n + n**2
 y6 = x**2 - 2*x*m + m**2
+dy1 = 2*x
+dy2 = dy1
+dy3 = 2*x + 1
+dy4 = 2*x - 1
 
 # Create the plot
 # plt.plot(n,x1, label="(2 - n^2)/2n")
@@ -30,12 +35,16 @@ y6 = x**2 - 2*x*m + m**2
 # plt.plot(x, eq1, label="x^2 + 2")
 # plt.plot(x, eq2, label="x^2 - 2 + x")
 # plt.plot(x, ysqr, label="x^2 + 2x + 1")
-# plt.plot(x, y1, label="A: x^2", linestyle='--', marker='o')
+plt.plot(x, y1, label="A: x^2", linestyle='--', marker='o')
 plt.plot(x, y2, label="B: x^2 + 2", linestyle='--', marker='v')
 # plt.plot(x, y3, label="C: x^2 - 2 + x", linestyle='--', marker='v')
 # plt.plot(x, y4, label="D: x^2 - 2 - x", linestyle='--', marker='v')
-plt.plot(x, y5, label="E: x^2 + 2xn + n^2", linestyle='--', marker='o')
+# plt.plot(x, y5, label="E: x^2 + 2xn + n^2", linestyle='--', marker='o')
 # plt.plot(x, y6, label="F: x^2 - 2xn + n^2", linestyle='--', marker='o')
+plt.plot(x, dy1, label="dA: 2x", linestyle='--', marker='x')
+# plt.plot(x, dy2, label="dB: 2x", linestyle='--', marker='x')
+plt.plot(x, dy3, label="dB: 2x + 1", linestyle='--', marker='x')
+plt.plot(x, dy4, label="dB: 2x - 1", linestyle='--', marker='x')
 
 # Create labels
 # plt.ylabel('x or y^2')
